LineageOS/android_hardware_qcom_bt

Add compat for old wcnss_filter
* New one has "vendor." prefix for all it's props.

Change-Id: Ifa0690bd1267cba9183396c8b6fe4a249a9ad9a4

libbt-vendor/Android.mk
@@ -99,6 +99,9 @@ endif
 ifeq ($(QCOM_BT_READ_ADDR_FROM_PROP),true)
 LOCAL_CFLAGS += -DREAD_BT_ADDR_FROM_PROP
 endif
+ifeq ($(QCOM_BT_USES_OLD_WCNSS_FILTER),true)
+LOCAL_CFLAGS += -DUSES_OLD_WCNSS_FILTER
+endif
 
 #include $(LOCAL_PATH)/vnd_buildcfg.mk
 

Elite-Project/hardware_qcom_bt

Signed-off-by: klozz <shaclone707@gmail.com>

libbt-vendor/Android.mk
@@ -102,6 +102,9 @@ endif
 ifeq ($(QCOM_BT_READ_ADDR_FROM_PROP),true)
 LOCAL_CFLAGS += -DREAD_BT_ADDR_FROM_PROP
 endif
+ifeq ($(QCOM_BT_USE_OLD_WCNSS_FILTER),true)
+LOCAL_CFLAGS += -DUSE_OLD_WCNSS_FILTER
+endif
 
 #include $(LOCAL_PATH)/vnd_buildcfg.mk
 